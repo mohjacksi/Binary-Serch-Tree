92c2d260bea7da6e3cd18ead560bd0dccbfc3e56
Mohammed Jacksi <mohjacksi@Mohammeds-MacBook-Air.local>
2017-02-08 07:17:46 +0300

    Initial Commit

---
+++ Binary Serch Tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Binary Serch Tree
+//
+//  Created by Mohammed Jacksi on 2/8/17.
+//  Copyright © 2017 Mohammed Jacksi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
